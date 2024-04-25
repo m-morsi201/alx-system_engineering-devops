13cc310df218f6042cc0465f15fb9ded34102fa7
Muhamad Morsy <muhammadmorsy5@gmail.com>
2024-04-24 19:54:36 -0700

    remove PasswordAuthentication no, and present,

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,16 +1,16 @@
 #set up my client SSH configuration file so that
 #can connect to a server without typing a password.
-include stdlib
-file_line { 'Turn off passwd auth':
-	ensure => 'present',
-	path   => '/etc/ssh/ssh_config',
-	line   => '    PasswordAuthentication no',
-	replace => true,
+file_line { 'SSH Private Key':
+  path               => '/etc/ssh/ssh_config',
+  line               => '    IdentityFile ~/.ssh/school',
+  match              => '^[#]+[\s]*(?i)IdentityFile[\s]+~/.ssh/id_rsa$',
+  replace            => true,
+  append_on_no_match => true,
 }
-
-file_line { 'Declare identity file':
-	ensure => 'present',
-	path   => '/etc/ssh/ssh_config',
-	line   => '    IdentityFile ~/.ssh/school',
-	replace => true,
+file_line { 'Deny Password Auth':
+  path               => '/etc/ssh/ssh_config',
+  line               => '    PasswordAuthentication no',
+  match              => '^[#]+[\s]*(?i)PasswordAuthentication[\s]+(yes|no)$',
+  replace            => true,
+  append_on_no_match => true,
 }
